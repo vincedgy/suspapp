d9fa5bddd2f1e806b38f1a7afc3e4ccec0a2cffd
Vincedgy <vincent.dagoury@gmail.com>
2020-05-16 09:51:22 +0200

    Adding test automation

--- src/App.js
+++ src/App.js
@@ -13,15 +13,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ErrorBoundary>
-          <Suspense fallback={<p>Wait a moment please...</p>}>
-            <Profile resource={resource} />
-          </Suspense>
-        </ErrorBoundary>
-        <Suspense fallback={<p>Wait a moment please...</p>}>
-          <Figure resource={resource} />
-        </Suspense>
+        <h1>Suspense !</h1>
       </header>
+
+      <ErrorBoundary>
+        <Suspense fallback={<p>Wait a moment please...</p>}>
+          <Profile resource={resource} />
+        </Suspense>
+      </ErrorBoundary>
+      <Suspense fallback={<p>Wait a moment please...</p>}>
+        <Figure resource={resource} />
+      </Suspense>
     </div>
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Profile = ({ resource }) => {
   const person = resource.person.read();
-  return <h2>Hello {`${person.name.first} ${person.name.last}`}</h2>;
+  return <h2>Your profile is {`${person.name.first} ${person.name.last}`}</h2>;
 };
 
 export default Profile;
